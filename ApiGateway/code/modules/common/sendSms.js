ca4c33b892dffa9e6982520f1a45cf36eb8848d7
cleanmycar <cleanmycar.tech@gmail.com>
2020-01-10 22:41:09 +0530

    register user with otp

--- ApiGateway/code/modules/common/sendSms.js
+++ ApiGateway/code/modules/common/sendSms.js
@@ -1,5 +1,16 @@
 module.exports = (config, params, callback) => {
-    callback(null, {
-        type: "success"
-    });
+    // let message = `Please use OTP ${params.userOtp} to reset your password in CleanMyCar`
+    var options = {
+        method: 'POST',
+        url: `https://www.bulksmsgateway.in/sendmessage.php?user=CleanMyCar&password=ANV@2019anv&message=${params.message}&sender=CMYCAR&mobile=${params.mobileNumber}&type=3`
+    };
+
+    request(options)
+        .then(function (parsedBody) {
+            return callback(null, result.recordsets[0]);
+        })
+        .catch(function (err) {
+            console.log(err)
+            return callback(err, result.recordsets[0]);
+        });
 }
--- ApiGateway/code/modules/user/registerUser.js
+++ ApiGateway/code/modules/user/registerUser.js
@@ -1,33 +1,72 @@
-var md5 = require('md5');
-const mssql = require('mssql');
-const generateOtp = require("./generateOtp")
+var md5 = require("md5");
+const mssql = require("mssql");
+const generateOtp = require("./generateOtp");
+const sendSms = require("../common/sendSms")
 
 module.exports = async (config, params, callback) => {
+  var userOtp = null;
+  var sp_name = "SaveRegisterUserDetail";
 
-    let userOtp = await generateOtp();
+  let saveuser = function(isMobileNumberVerified) {
     const requestParams = config.dbwrapper.getNewRequest();
-    requestParams.input('UserId', mssql.Int, params.UserId);
-    requestParams.input('UserLastName', mssql.NVarChar, params.lastName);
-    requestParams.input('UserFirstName', mssql.NVarChar, params.firstName);
-    requestParams.input('Email', mssql.NVarChar, params.email);
-    requestParams.input('UserPassword', mssql.NVarChar, md5(params.password));
-    requestParams.input('StatusId', mssql.Int, 1);
-    requestParams.input('LoggedinUser', mssql.Int, 1);
-    requestParams.input('ProfileImage', mssql.NVarChar, params.profileImage);
-    requestParams.input('MobileNumber', mssql.NVarChar, params.mobileNumber);
-    requestParams.input('ReferralCode', mssql.NVarChar, params.referralCode);
-    requestParams.input('UserOtp', mssql.NVarChar, userOtp);
-    requestParams.input('UserRoleId', mssql.Int, params.userRoleId ? params.userRoleId : 2);
+    requestParams.input("UserId", mssql.Int, params.UserId);
+    requestParams.input("UserLastName", mssql.NVarChar, params.lastName);
+    requestParams.input("UserFirstName", mssql.NVarChar, params.firstName);
+    requestParams.input("Email", mssql.NVarChar, params.email);
+    requestParams.input("UserPassword", mssql.NVarChar, md5(params.password));
+    requestParams.input("StatusId", mssql.Int, 1);
+    requestParams.input("LoggedinUser", mssql.Int, 1);
+    requestParams.input("ProfileImage", mssql.NVarChar, params.profileImage);
+    requestParams.input("MobileNumber", mssql.NVarChar, params.mobileNumber);
+    requestParams.input("ReferralCode", mssql.NVarChar, params.referralCode);
+    requestParams.input("UserOtp", mssql.NVarChar, userOtp);
+    requestParams.input(
+      "UserRoleId",
+      mssql.Int,
+      params.userRoleId ? params.userRoleId : 2
+    );
+    requestParams.input("IsMobileNumberVerified", mssql.Int, isMobileNumberVerified);
 
+    requestParams.execute(sp_name, (err, result) => {
+      if (err) {
+        console.log(err);
+        callback(err);
+      }
+      callback(null, result.recordsets);
 
-    requestParams.execute('SaveUserDetail', (err, result) => {
-        if (err) {
-            console.log(err);
-            callback(err);
-        }
-        callback(null, result.recordsets);
-
-        //Send E-mail and SMS about registration
-
+      //Send E-mail and SMS about registration
     });
+  };
+
+  let valdiateRegisteredUserOtp = function(){
+    const requestParams = config.dbwrapper.getNewRequest();
+    requestParams.input("MobileNumber", mssql.NVarChar, params.mobileNumber);
+    requestParams.input("Otp", mssql.NVarChar, params.userOtp);
+    requestParams.execute("ValidateRegisterUserOtp", (err, result) => {
+        if (err) {
+          console.log(err);
+          callback(err);
+        }   
+        if(result.recordsets[0] && result.recordsets[0][0] && result.recordsets[0][0].UserId){
+            saveuser(1);
+        }
+        else{
+            callback({ErrorMessage: "User Not found"})
+        }        
+      });
+    
+  }
+
+  if (params.isVerify) {    
+    sp_name = "SaveUserDetail";
+    valdiateRegisteredUserOtp();    
+  }
+  else {
+    userOtp = await generateOtp();
+    saveuser();
+    sendSms(config, {
+        message: `Please use OTP ${userOtp} to reset your password in CleanMyCar`,
+        mobileNumber: params.mobileNumber
+    })
+  }
 };
